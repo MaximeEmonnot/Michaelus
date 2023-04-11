2bf019cdb4480c386846a8c9d10171386b79375f
Maxime Emonnot <61348720+MaximeEmonnot@users.noreply.github.com>
2023-04-11 17:57:14 +0200

    Ajout de commentaires pour la partie AudioEngine

--- Michaelus/SoundSystem.cpp
+++ Michaelus/SoundSystem.cpp
@@ -2,32 +2,45 @@
 
 #include <vector>
 
+// VARIABLES STATIQUES
 std::unique_ptr<SoundSystem> SoundSystem::pInstance = nullptr;
 
+// ********* //
+
+// Constructeur
 SoundSystem::SoundSystem()
 {
+	// On ouvre le Device de OpenAL
 	pAlDevice = alcOpenDevice(nullptr);
 	if (pAlDevice)
 	{
+		// Si le Device est bien ouvert, on crée le contexte permettant de jouer les différents sons
 		pAlContext = alcCreateContext(pAlDevice, nullptr);
 		alcMakeContextCurrent(pAlContext);
 	}
 
+	// On initialise les différentes sources qui composeront le système OpenAL
 	sources.resize(nbSources);
 	alGenSources(nbSources, sources.data());
 }
 
+// Destructeur
 SoundSystem::~SoundSystem()
 {
+	// On supprime les sources qui composent le système OpenAL
 	alDeleteSources(static_cast<ALint>(sources.size()), sources.data());
 	for (auto& entry : soundBuffers)
 		alDeleteBuffers(1, &entry.second);
 
+	// On détruit le contexte permettant de jouer les différents sons
 	alcMakeContextCurrent(nullptr);
 	alcDestroyContext(pAlContext);
+
+	// On ferme le Device OpenAL
 	alcCloseDevice(pAlDevice);
 }
 
+// Fonction propre au patron de conception Singleton
 SoundSystem& SoundSystem::GetInstance()
 {
 	if (!pInstance)
@@ -35,50 +48,64 @@ SoundSystem& SoundSystem::GetInstance()
 	return *pInstance;
 }
 
+// Mise à jour d'un son en cours de lecture
 void SoundSystem::UpdatePlayingSound(int index, bool bIsLooping, float pitch, float gain, const FVec3D& location,
 	const FVec3D& velocity)
 {
+	// On récupère la source correspondant à l'index
 	ALuint source = sources.at(index);
 
+	// Si la source est bien en état de lecture, on met à jour les paramètres suivants :
 	if (GetSourceStatus(source) == AL_PLAYING)
 	{
-		alSourcei(source, AL_LOOPING, bIsLooping);
-		alSourcef(source, AL_PITCH, pitch);
-		alSourcef(source, AL_GAIN, gain);
-		alSource3f(source, AL_POSITION, location.GetY(), location.GetX(), location.GetZ());
-		alSource3f(source, AL_VELOCITY, velocity.GetY(), velocity.GetX(), velocity.GetZ());
+		alSourcei(source, AL_LOOPING, bIsLooping); // Paramètre de boucle
+		alSourcef(source, AL_PITCH, pitch); // Paramètre de hauteur
+		alSourcef(source, AL_GAIN, gain); // Paramètre de gain
+		alSource3f(source, AL_POSITION, location.GetY(), location.GetX(), location.GetZ()); // Paramètre de position
+		alSource3f(source, AL_VELOCITY, velocity.GetY(), velocity.GetX(), velocity.GetZ()); // Paramètre de vélocité
 	}
 }
 
+// Mise en pause d'un son
 void SoundSystem::PauseSound(int index)
 {
+	// On récupère la source correspondant à l'index
 	ALuint source = sources.at(index);
 
+	// Si la source est bien en état de lecture, on met en pause la lecture
 	if (GetSourceStatus(source) == AL_PLAYING)
 		alSourcePause(source);
 }
 
+// Reprise d'un son
 void SoundSystem::ResumeSound(int index)
 {
+	// On récupère la source correspondant à l'index
 	ALuint source = sources.at(index);
 
+	// Si la source est bien en état de pause, on reprend la lecture
 	if (GetSourceStatus(source) == AL_PAUSED) 
 		alSourcePlay(source);
 }
 
+// Arrêt d'un son
 void SoundSystem::StopSound(int index)
 {
+	// On récupère la source correspondant à l'index
 	ALuint source = sources.at(index);
 
+	// Si la source n'est pas en état d'arrêt, on arrête la lecture
 	if (GetSourceStatus(source) != AL_STOPPED)
 		alSourceStop(source);
 }
 
+// Définition de la position du listener
 void SoundSystem::SetListenerLocation(const FVec3D& location)
 {
 	alListener3f(AL_POSITION, location.GetY(), location.GetX(), location.GetZ());
 }
 
+// Récupération du statut d'une source (Etat Initial, En pause, En cours de lecture, A l'arrêt)
 ALint SoundSystem::GetSourceStatus(ALuint source)
 {
 	ALint status;
--- Michaelus/SoundSystem.h
+++ Michaelus/SoundSystem.h
@@ -13,9 +13,17 @@
 #define SFX SoundSystem::GetInstance()
 
 
+/*
+ * Classe d'un son, joué par la bibliothèque OpenAL
+ * Permet de récupérer les données d'un fichier .WAV
+ */
 class Sound
 {
 protected:
+	/*
+	 * Données d'un fichier .WAV
+	 * Définit le nombre de samples, de taux de sampling, et les samples du fichier
+	 */
 	struct SoundData
 	{
 		ALsizei nbSamples;
@@ -25,25 +33,34 @@ protected:
 
 	static SoundData GetSoundData(const std::string& path)
 	{
+		// On ouvre le fichier .wav
 		SoundData out;
 		SF_INFO fileInfos;
 		SNDFILE* file = sf_open(path.c_str(), SFM_READ, &fileInfos);
 		if (!file) throw 0;
 
+		// On définit le nombre de sample et le taux de sampling
 		out.nbSamples = static_cast<ALsizei>(fileInfos.channels * fileInfos.frames);
 		out.sampleRate = static_cast<ALsizei>(fileInfos.samplerate);
 
+		// On définit les samples
 		out.samples.resize(out.nbSamples);
 		if (sf_read_short(file, out.samples.data(), out.nbSamples) < out.nbSamples) throw 0;
 
+		// On ferme le fichier
 		sf_close(file);
 
 		return out;
 	}
 };
 
+/*
+ * Classe Effect, définissant un son pouvant être spatialisé
+ * Attention toute fois : OpenAL ne gère la spatialisation qu'avec des sons MONO
+ */
 class Effect : private Sound{
 public:
+	// Chargement du son : On définit les données du buffer audio via les données du fichier .WAV, puis on retourne ce buffer qui sera engistré plus tard
 	static ALuint LoadSound(const std::string& path)
 	{
 		SoundData data = GetSoundData(path);
@@ -59,9 +76,14 @@ public:
 	}
 };
 
+/*
+ * Classe Music, définissant un son pouvant être joué en boucle
+ * Ce son est STEREO, et ne peut pas être spatialisé
+ */
 class Music : private Sound
 {
 public:
+	// Chargement du son : On définit les données du buffer audio via les données du fichier .WAV, puis on retourne ce buffer qui sera engistré plus tard
 	static ALuint LoadSound(const std::string& path)
 	{
 		SoundData data = GetSoundData(path);
@@ -77,6 +99,10 @@ public:
 	}
 };
 
+/*
+ * Structure SoundKey
+ * Clé de la map de sons
+ */
 struct SoundKey
 {
 	class Comparator
@@ -92,30 +118,53 @@ struct SoundKey
 	std::string path;
 };
 
+/*
+ * Système audio du moteur géré par OpenAL
+ * OpenAL permet la gestion d'un unique listener (l'utilisateur de l'application) et de plusieurs sources audio (les sons que l'on jouera)
+ */
 class SoundSystem
 {
 public:
+	// Constructeur (public car std::unique_ptr nécessite un constructeur publique) 
 	SoundSystem();
+	// Destructeur
 	~SoundSystem();
 
+	// Ces différents constructeurs sont définis comme étant supprimés, pour correspondre au patron de conception Singleton
+	// et également à la RO5 des normes du C++
+	SoundSystem(const SoundSystem&) = delete;
+	SoundSystem& operator=(const SoundSystem&) = delete;
+	SoundSystem(SoundSystem&&) = delete;
+	SoundSystem& operator=(SoundSystem&&) = delete;
+
+	// Méthode du patron de conception Singleton
 	static SoundSystem& GetInstance();
 
+	/*
+	 * Fonction permettant de jouer un son
+	 * Prends comme paramètre le chemin du son, si l'on le joue en boucle, sa hauteur, son gain
+	 * sa position, et sa vélocité. Ces autres paramètres sont facultatifs.
+	 */
 	template<class SoundType>
 	int PlaySound(const std::string& path, bool bIsLooping = false, float pitch = 1.f, float gain = 1.f, 
 		const FVec3D& location = CAMERA.GetActiveCamera()->GetWorldLocation(), const FVec3D& velocity = FVec3D())
 	{
+		// On ouvre (ou récupère) le son lié au path
 		SoundKey key = { 0, path };
 		if (!soundBuffers.contains(key)) soundBuffers.emplace(key, SoundType::LoadSound(path));
 
+		// On parcourt donc l'ensemble des sources du système...
 		for (size_t i = 0; i < sources.size(); i++)
 		{
 			ALuint source = sources.at(i);
-
+			// ...et on vérifie si la source est soit en état initial soit arrêtée...
 			switch(GetSourceStatus(source))
 			{
 			case AL_INITIAL:
 			case AL_STOPPED:
 				{
+				// ...puis on définit tous les paramètres (Boucle, Hauteur, Gain, Position, Velocité, Source sélectionnée), avant de lancer le son
+				// et de retourner l'index de la source pour l'altérer plus tard
 				alSourcei(source, AL_LOOPING, bIsLooping);
 				alSourcef(source, AL_PITCH, pitch);
 				alSourcef(source, AL_GAIN, gain);
@@ -129,19 +178,26 @@ public:
 				break;
 			}
 		}
+		// Si aucune sourcen n'est disponible, on renvoie -1
 		return -1;
 	}
 
+	// Mise à jour d'un son en cours de lecture
 	void UpdatePlayingSound(int index, bool bIsLooping = false, float pitch = 1.f, float gain = 1.f,
 		const FVec3D& location = CAMERA.GetActiveCamera()->GetWorldLocation(), const FVec3D& velocity = FVec3D());
 
+	// Mise en pause d'un son
 	void PauseSound(int index);
+	// Reprise d'un son
 	void ResumeSound(int index);
+	// Arrêt d'un son
 	void StopSound(int index);
 
+	// Définition de la position du listener
 	void SetListenerLocation(const FVec3D& location);
 
 private:
+	// Récupération du statut d'une source (Etat Initial, En pause, En cours de lecture, A l'arrêt)
 	static ALint GetSourceStatus(ALuint source);
 
 private:
